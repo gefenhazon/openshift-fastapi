8b800d651b7b2a85c36a6ce5ee03fb1aba77284d
gefenhazon <gefenhazon@mail.tau.ac.il>
2023-11-06 10:11:16 +0200

    LOL

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ import uvicorn
 
 app = FastAPI()
 
+
 @app.get('/')
 def index():
-    return {'data': 'Hello FastAPI!'}
+    return "hi"
+
 
 @app.get('/message')
 def index():
     return {'data': 'FastAPI is great!'}
+
+
+@app.get('/lol')
+def index():
+    return "LOL"
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=8080)
